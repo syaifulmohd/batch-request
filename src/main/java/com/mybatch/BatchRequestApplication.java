b057cef5a5a37f64c173db074083b729ffc2bce4
syaifulmohd <syaiful.arqam@gmail.com>
2023-06-04 00:52:16 +0800

    initial commit

---
+++ src/main/java/com/mybatch/BatchRequestApplication.java
@@ -0,0 +1,13 @@
+package com.mybatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchRequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mybatch/BatchRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
